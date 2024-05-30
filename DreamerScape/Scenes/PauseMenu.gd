5b87789c859ffde66cfa183dacb7486b0c10c23e
Deo2003 <48349866+Deo2003@users.noreply.github.com>
2024-05-30 09:21:05 -0400

    Pause menu working

--- DreamerScape/Scenes/PauseMenu.gd
+++ DreamerScape/Scenes/PauseMenu.gd
@@ -1,8 +1,6 @@
 # PauseMenu.gd
 extends Control
 
-var is_paused: bool = false
-
 func _ready() -> void:
 	self.visible = false  # Initially hide the pause menu
 	$Panel/VBoxContainer/Mute.connect("pressed", Callable(self, "_on_mute_pressed"))
@@ -11,22 +9,28 @@ func _ready() -> void:
 
 func _input(event):
 	if event.is_action_pressed("pause"):
-		_toggle_pause()
+		if get_tree().paused:
+			_on_close_pressed()
+		else:
+			_on_pause_button_pressed()
 
-func _toggle_pause():
-	is_paused = not is_paused
-	get_tree().paused = is_paused
-	self.visible = is_paused
-	print("Paused:", is_paused)  # Debugging statement
+func _on_pause_button_pressed() -> void:
+	get_tree().paused = true
+	show()
 
-func _on_quit_pressed():
-	get_tree().quit()
-
-func _on_home_pressed():
-	get_tree().paused = false
-	get_tree().change_scene("res://Scenes/TitleScreen.tscn")
-
-func _on_mute_pressed():
+func _on_mute_pressed() -> void:
 	var master_bus = AudioServer.get_bus_index("Master")
 	var is_muted = AudioServer.is_bus_mute(master_bus)
 	AudioServer.set_bus_mute(master_bus, not is_muted)
+
+func _on_home_pressed() -> void:
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://Scenes/TitleScreen.tscn")
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_close_pressed():
+	hide()
+	get_tree().paused = false
